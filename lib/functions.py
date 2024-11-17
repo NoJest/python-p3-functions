36c1c34111941f9d1c0d106738e303ce427b4745
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-17 14:14:23 -0500

    this is it

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    return print(f"Hello, programmer!")
     pass
 
-def greet(name):
+def greet(name="guido"):
+    return print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    return print(f"Hello, {name}!")
     pass
 
-def add(num1, num2):
+def add(num1=44, num2=55):
+    return num1 + num2
     pass
 
-def halve(number):
+def halve(number=100):
+    return number / 2
     pass
